8b25aabdb93f48e3c48510f47ae21d08ca776e5b
Juan Cruz Lima Caramico <juan.mdq22@gmail.com>
2023-06-24 19:15:41 -0300

    working on playlist

--- components/Playlist.jsx
+++ components/Playlist.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import playlistHeaderImage from "../src/app/img/playlistHeaderImage.png";
 import Image from "next/image";
+import button from "../src/app/img/button.png";
 const Playlist = () => {
   return (
     <div className="w-full h-full text-[#FFFFFF]">
       <header className="h-2/5 bg-headerPlaylist-gradient flex items-end p-4">
-        <Image src={playlistHeaderImage} alt="img" className="w-auto h-52"/>
+        <Image src={playlistHeaderImage} alt="img" className="w-auto h-52" />
         <div className="flex flex-col gap-3">
           <h6>Playlist</h6>
           <h1 className="text-7xl font-bold">Pop Right Now</h1>
@@ -13,11 +14,15 @@ const Playlist = () => {
             Lorem ipsum dolor sit amet, consectetur adipiscing elit.
           </p>
           <p>
-            <span className="text-bold">Spotify</span> - <span className="opacity-70">860305 likes</span> -{" "}
+            <span className="text-bold">Spotify</span> -{" "}
+            <span className="opacity-70">860305 likes</span> -{" "}
             <span className="opacity-70">100 songs</span>
           </p>
         </div>
       </header>
+      <nav className="h-20 bg-slate-500">
+        <Image src={button} alt="button" />
+      </nav>
     </div>
   );
 };
