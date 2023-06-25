c4506b0430a67ff2048b6271b54c0a8fb5dd4a14
ZeroSwordDev <Miguelworking1@gmail.com>
2023-06-24 23:32:30 -0400

    usuario login y logout, tambien agregamos playlist , recent payed los hits, y fetaureds

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,6 @@
+import AuthProvider from "../../components/AuthProvider";
+import AuthContextProvider from "../../context/AuthContext";
+import ReleaseContextProvider from "../../context/NewReleases";
 import "./globals.css";
 import { Inter } from "next/font/google";
 
@@ -6,7 +9,13 @@ const inter = Inter({ subsets: ["latin"] });
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <AuthProvider>
+          <AuthContextProvider>
+            <ReleaseContextProvider>{children}</ReleaseContextProvider>
+          </AuthContextProvider>
+        </AuthProvider>
+      </body>
     </html>
   );
 }
