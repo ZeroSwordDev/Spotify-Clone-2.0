da0e8386c9676e3b4b213499c977fa374b9e3d62
ZeroSwordDev <Miguelworking1@gmail.com>
2023-06-26 16:35:33 -0400

    usuario login y logout, tambien agregamos playlist , recent payed los hits, y fetaureds

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -57,7 +57,6 @@ const handler = NextAuth({
   callbacks: {
     async jwt({ token, account }) {
       // Persist the OAuth access_token to the token right after signin
-
       if (account) {
         token.accessToken = account.access_token;
         token.refreshToken = account.refresh_token;
